392de0ec1d086edc33f5282cb9055b6d4b52adfc
Daniel JAFFAL <daniel.jaffal@epita.fr>
2022-11-02 21:25:15 +0100

    First commit

--- expense.py
+++ expense.py
@@ -1,4 +1,5 @@
 from PyInquirer import prompt
+import csv
 
 expense_questions = [
     {
@@ -16,14 +17,20 @@ expense_questions = [
         "name":"spender",
         "message":"New Expense - Spender: ",
     },
-
 ]
 
 
 
 def new_expense(*args):
     infos = prompt(expense_questions)
+    with open('users.csv') as file:
+        if infos.get('spender') not in [line.rstrip() for line in file]:
+            print("Spender should be chosen among existing users !")
+            return
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    with open('expense_report.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow([infos.get('amount'), infos.get('label'), infos.get('spender')])
     print("Expense Added !")
     return True
 
--- user.py
+++ user.py
@@ -1,8 +1,20 @@
 from PyInquirer import prompt
-user_questions = [
+import csv
 
+user_questions = [
+    {
+        "type":"input",
+        "name":"name",
+        "message":"New User - Name: ",
+    },
 ]
 
-def add_user():
+def add_user(*args):
     # This function should create a new user, asking for its name
-    return
+    infos = prompt(user_questions)
+    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    with open('users.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow([infos.get('name')])
+    print("User Added !")
+    return True
