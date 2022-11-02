90ff80680d2c32ce43e8181cbbfbe6e9c289f828
Daniel JAFFAL <daniel.jaffal@epita.fr>
2022-11-02 22:28:21 +0100

    Final commit

--- expense.py
+++ expense.py
@@ -23,14 +23,25 @@ expense_questions = [
 
 def new_expense(*args):
     infos = prompt(expense_questions)
-    with open('users.csv') as file:
-        if infos.get('spender') not in [line.rstrip() for line in file]:
+    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    users = []
+    with open('users.csv','r') as file:
+        users = file.readlines()
+        if infos.get('spender') not in [user.rstrip() for user in users]:
             print("Spender should be chosen among existing users !")
             return
-    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
-    with open('expense_report.csv', 'a') as file:
-        writer = csv.writer(file)
-        writer.writerow([infos.get('amount'), infos.get('label'), infos.get('spender')])
+    for user in users:
+        user.rstrip()
+    with open('expense_report.csv', 'a') as f:
+        writer = csv.writer(f)
+        if len(users) == 1:
+            writer.writerow([infos.get('amount'), infos.get('label'), infos.get('spender')])
+        else:
+            for user in users:
+                if (user.rstrip() == infos.get('spender')):
+                    writer.writerow([infos.get('amount'), infos.get('label'), infos.get('spender')])
+                else:
+                    writer.writerow([int(infos.get('amount'))//((len(users)-1 )* -1), infos.get('label'), user.rstrip()])
     print("Expense Added !")
     return True
 
